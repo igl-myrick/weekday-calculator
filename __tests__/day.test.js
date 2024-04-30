4c1a83df508058f47b681bef3dfe3a9fb2a02521
igl-myrick <igl.myrick@gmail.com>
2024-04-30 15:02:57 -0700

    update second test for day object

--- __tests__/day.test.js
+++ __tests__/day.test.js
@@ -1,14 +1,9 @@
 import Day from '../src/day.js'
 
 describe('Day', () => {
-  
-  test('should correctly create a Day object from the current time', () => {
-    let today = new Day();
-    expect(today.date).toEqual("04/30/2024");
-  });
 
   test('should return a date based on the object parameters', () => {
     let newDay = new Day("04/22/1998");
-    expect(newDay.date).toEqual("April 22, 1998");
+    expect(newDay.date).toEqual("Wed Apr 22 1998");
   });
 });
