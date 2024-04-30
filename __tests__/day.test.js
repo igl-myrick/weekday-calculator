765f08488dd039693dddc1c5283c712570113be3
igl-myrick <igl.myrick@gmail.com>
2024-04-30 14:46:59 -0700

    add second test for Day object

--- __tests__/day.test.js
+++ __tests__/day.test.js
@@ -6,4 +6,9 @@ describe('Day', () => {
     let today = new Day();
     expect(today.date).toEqual("04/30/2024");
   });
+
+  test('should return a date based on the object parameters', () => {
+    let newDay = new Day("04/22/1998");
+    expect(newDay.date).toEqual("April 22, 1998");
+  });
 });
