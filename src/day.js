7b6bb608397192af534f1f63d49b97e2b0a24ec0
igl-myrick <igl.myrick@gmail.com>
2024-05-03 13:51:23 -0700

    add getWeekday prototype

--- src/day.js
+++ src/day.js
@@ -1,5 +1,25 @@
 export default class Day {
   constructor(inputDate) {
-    this.date = new Date(inputDate).toDateString();
+    this.date = new Date(inputDate);
+    this.dateStr = this.date.toDateString().slice(4);
+  }
+
+  getWeekday() {
+    const weekday = this.date.getDay();
+    if (weekday === 0) {
+      return "Sunday";
+    } else if (weekday === 1) {
+      return "Monday";
+    } else if (weekday === 2) {
+      return "Tuesday";
+    } else if (weekday === 3) {
+      return "Wednesday";
+    } else if (weekday === 4) {
+      return "Thursday";
+    } else if (weekday === 5) {
+      return "Friday";
+    } else if (weekday === 6) {
+      return "Saturday";
+    }
   }
 }
