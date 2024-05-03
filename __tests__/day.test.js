0a037b804e29a1823683a5663294bba620da900e
igl-myrick <igl.myrick@gmail.com>
2024-05-03 13:52:02 -0700

    update test for getWeekday prototype

--- __tests__/day.test.js
+++ __tests__/day.test.js
@@ -1,9 +1,18 @@
 import Day from '../src/day.js'
 
-describe('Day', () => {
+describe("Day", () => {
 
-  test('should return a date based on the object parameters', () => {
+  test("should return a date based on the object parameters", () => {
     let newDay = new Day("04/22/1998");
-    expect(newDay.date).toEqual("Wed Apr 22 1998");
+    expect(newDay.dateStr).toEqual("Apr 22 1998");
+  });
+});
+
+describe("Day.prototype.getWeekday", () => {
+
+  test("should return a day of the week according to the object's value", () => {
+    let newDay = new Day("04/22/1998");
+    const weekday = newDay.getWeekday();
+    expect(weekday).toEqual("Wednesday")
   });
 });
